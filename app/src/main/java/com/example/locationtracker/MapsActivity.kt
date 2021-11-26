3fbc2050180fdae390b069336a538658c34973cb
Bak Dávid <bakdavid021@gmail.com>
2021-11-26 13:32:33 +0100

    feat: Now able to get user's current location

--- app/src/main/java/com/example/locationtracker/MapsActivity.kt
+++ app/src/main/java/com/example/locationtracker/MapsActivity.kt
@@ -1,25 +1,22 @@
 package com.example.locationtracker
 
-import android.content.ContentValues.TAG
+
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-
-
+import com.example.locationtracker.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.locationtracker.databinding.ActivityMapsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -63,8 +60,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun requestLocationPermissions() {
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION) //helymeghatározás engedélykérés
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION) //helymeghatározás engedélykérés
     }
 
-    
+    private fun getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermissions()
+        }
+        else {
+            fusedLocClient.lastLocation.addOnCompleteListener {
+                val location = it.result
+                val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+                val ref: DatabaseReference = database.getReference("test")
+                if (location != null) {
+                    val latLng = LatLng(location.latitude, location.longitude)
+
+                    map.addMarker(MarkerOptions().position(latLng) // Marker létrehozása az adott helyen
+                        .title("Itt vagy jelenleg!"))
+
+                    val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
+
+                    map.moveCamera(update)
+
+                    ref.setValue(location) //helyadat elmentése az adatbázisba
+                }
+                else {
+                    Log.e(TAG, "Hely nem található!")
+                }
+            }
+        }
+    }
+
 }
